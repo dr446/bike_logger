0a54a55cde16cdffb7b2481354892b6197dad349
Diya <dr446@cam.ac.uk>
2018-05-17 10:10:40 +0100

    arduino stuff added

--- SD_card_write_test/SD_card_write_test.ino
+++ SD_card_write_test/SD_card_write_test.ino
@@ -3,29 +3,65 @@
 #include <SPI.h>
 #include <SD.h>
 
+//SD card attached to SPI bus as follows:
+//MOSI - pin 11
+//MISO - pin 12
+//CLK - pin 13
+//CS - pin 4
 
 void setup() {
-
-SD_begin(9600); //Initialises communication between arduino and computer to run at 9600 bits/second
-
-
-}
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 
 void loop() {
 
-float time_unit = 0.1; //actual time needs to be put in here
-float current_speed  = 5; //output from hall effect sensor calculation firmware
-String data_string = "";
+String mock_data = "";
 
-data_string += String(time_unit);
-data_string += String(current_speed);
-File dataFile = SD.open("datalog.txt", FILE_WRITE);
+Serial.print("Initializing SD card...");
 
- if (dataFile) {
-  
-    dataFile.println(data_string);
-    dataFile.close();
-
+// see if the card is present and can be initialized:
+if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
   }
+  Serial.println("card initialized.");
+}
+
+//load mock data from matlab and send it to SD card. This section will have to be modified to send the speed calcs from the sensors to the SD card.
+
+while (Serial.available() == 0)
+    {
+    }
+    if (Serial.available() > 0)
+    {
+      for (int i=0; i < 2 ; i++)
+      {
+          mock_data += string(Serial.read());
+      }
+      Serial.flush();
+       mock_data +=",";
+   }
+
+
+// open the file. note that only one file can be open at a time,
+// so you have to close this one before opening another.
+File dataFile = SD.open("speed.txt", FILE_WRITE);
+
+// if the file is available, write to it:
+if (dataFile) {
+   dataFile.println(mock_data);
+   dataFile.close();
+   // print to the serial port too:
+   Serial.println(mock_data);
+ }
+ // if the file isn't open, pop up an error:
+ else {
+   Serial.println("error opening datalog.txt");
+ }
+}
 
 }
